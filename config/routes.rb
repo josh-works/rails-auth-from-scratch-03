0a1b12f3a5e74d44cb212634c55c46c28b374755
Josh Thompson <thompsonjoshd@gmail.com>
2022-02-10 21:52:06 -0700

    Set root to book_quotes#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :book_quotes
+  root to: 'book_quotes#index'
+  resources :users, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
