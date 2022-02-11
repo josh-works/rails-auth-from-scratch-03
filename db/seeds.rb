514f46a37555668103f2433c3f69b4a1c562e194
Josh Thompson <thompsonjoshd@gmail.com>
2022-02-10 21:53:18 -0700

    first stab at seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+5.times do
+  User.find_or_create_by(
+    email: Faker::Internet.email,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    avatar: Faker::Avatar.image(size: "50x50", format: "jpg"),
+    # password_digest: "password"
+  )
+end
+
+100.times do 
+  quote = Faker::Movie.quote
+  book_title = Faker::Book.title
+  submitted_by = User.order("RANDOM()").first
+  BookQuote.create(quote: quote, book_title: book_title, user: submitted_by)
+end
+
