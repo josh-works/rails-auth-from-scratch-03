c628e0af29e292e3742646a92db692ea637efa79
Josh Thompson <thompsonjoshd@gmail.com>
2022-02-10 22:58:31 -0700

    Finish step 4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Success! Welcome new user"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def new
     @user = User.new
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:first_name, :email, :password, :password_confirmation)
   end
 end
