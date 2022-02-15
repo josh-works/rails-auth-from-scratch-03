0130dcd18b7c8ef40cf057d676ba0a9f4fc1f0e5
Josh Thompson <thompsonjoshd@gmail.com>
2022-02-14 18:15:48 -0700

    Got it working

--- app/controllers/strava_runs_controller.rb
+++ app/controllers/strava_runs_controller.rb
@@ -2,13 +2,16 @@ require 'csv'
 class StravaRunsController < ApplicationController
   def index
     @runs = get_run_data
-    
   end
   
   private
   
   def get_run_data
-    data = CSV.open('runs.csv').read
-    data.to_json
+    # data = CSV.open('runs.csv', symbolize).read
+    # data.to_json
+    
+    poly = "".to_json
+    
+    [poly]
   end
 end
